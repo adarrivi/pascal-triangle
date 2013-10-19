40dd5d19fb671c391172712ba5f05c86a3cec769
adarrivi@gmail.com <adarrivi@gmail.com>
2013-10-19 12:59:58 +0100

    Adding simple cache. Changeching to algorithm solution as formula with factorials cannot handle properly more than 35 levels.

--- pascal-triangle/src/main/java/com/pascal/triangle/config/SpringApplicationContext.java
+++ pascal-triangle/src/main/java/com/pascal/triangle/config/SpringApplicationContext.java
@@ -1,6 +1,12 @@
 package com.pascal.triangle.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +15,7 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @ComponentScan("com.pascal.triangle")
 @PropertySource("classpath:server.properties")
+@EnableCaching
 public class SpringApplicationContext {
 
 	@Bean
@@ -17,6 +24,14 @@ public class SpringApplicationContext {
 		serviceLocatorFactoryBean
 				.setServiceLocatorInterface(HttpControllerFactory.class);
 		return serviceLocatorFactoryBean;
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		SimpleCacheManager cacheManager = new SimpleCacheManager();
+		cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache(
+				"getHumanWeightOverShoulders")));
+		return cacheManager;
 	}
 
 }
--- pascal-triangle/src/main/java/com/pascal/triangle/service/impl/HumanPyramidServiceDefaultImpl.java
+++ pascal-triangle/src/main/java/com/pascal/triangle/service/impl/HumanPyramidServiceDefaultImpl.java
@@ -1,7 +1,10 @@
 package com.pascal.triangle.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.pascal.triangle.model.HumanPyramid;
@@ -11,21 +14,28 @@ import com.pascal.triangle.service.HumanPyramidService;
 @Service
 public class HumanPyramidServiceDefaultImpl implements HumanPyramidService {
 
+	private static final Logger LOG = LoggerFactory
+			.getLogger(HumanPyramidServiceDefaultImpl.class);
+
 	private static final int EDGE_HUMAN_INDEX = 0;
 	private static final int DEFAULT_WEIGHT = 50;
 
 	@Autowired
-	@Qualifier("formulaWeightPascalTriangleCalculator")
+	@Qualifier("algorithmWeightPascalTriangleCalculator")
 	private WeightPascalTriangleCalculator weightPascalTriangleCalculator;
 
 	@Override
+	@Cacheable(value = "getHumanWeightOverShoulders")
 	public String getHumanWeightOverShoulders(int levelIndex,
 			Integer optionalHumanIndex) {
 		HumanPyramid humanPyramid = new HumanPyramid(
 				weightPascalTriangleCalculator, DEFAULT_WEIGHT);
 		int humanIndex = optionalHumanIndex == null ? EDGE_HUMAN_INDEX
 				: optionalHumanIndex.intValue();
+		long currentTimeMillis = System.currentTimeMillis();
 		double weight = humanPyramid.getHumanEdgeWeight(levelIndex, humanIndex);
+		currentTimeMillis = System.currentTimeMillis() - currentTimeMillis;
+		LOG.debug("The operation took {} milliseconds", currentTimeMillis);
 		return Double.toString(weight);
 	}
 
